d9e54aae0b3f687b2c36aa4dc3cd5edb42e8632b
Derrick Kim <derrickkim0109@naver.com>
2022-02-19 12:35:10 +0900

    feat : 'OrderViewController' tagButton 및 재고량 세팅

--- JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderViewController.swift
@@ -22,5 +22,25 @@ class OrderViewController: UIViewController {
     /// 주문할 쥬스 선택버튼
     @IBAction func tagOrderButton(_ sender: UIButton) {
         let selectedJuice = Juice(rawValue: sender.tag) ?? .strawberry
+        
+        let result = juiceMaker.make(juice: selectedJuice)
+
+        switch result {
+        case .success(let juice):
+            break
+        case .failure(let notEnoughFruits):
+            break
+        }
+    }
+}
+
+extension OrderViewController {
+    /// 현재 재고량
+    private func setCurrentAmount() {
+        strawberryLabel.text = String(juiceMaker.count(of: .strawberry))
+        bananaLabel.text = String(juiceMaker.count(of: .banana))
+        pineappleLabel.text = String(juiceMaker.count(of: .pineapple))
+        kiwiLabel.text = String(juiceMaker.count(of: .kiwi))
+        mangoLabel.text = String(juiceMaker.count(of: .mango))
     }
 }
